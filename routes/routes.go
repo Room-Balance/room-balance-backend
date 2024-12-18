bd01f716efe20ac9dc66dd09fd79145183b77f22
Batuhan Özender <b.ozender59@gmail.com>
2024-12-18 07:23:57 +0300

    Set up routes for users and update .gitignore

--- routes/routes.go
+++ routes/routes.go
@@ -1,8 +1,20 @@
 package routes
 
-import "fmt"
+import (
+	"github.com/Room-Balance/room-balance-backend.git/handlers"
+	"github.com/gorilla/mux"
+)
 
-// InitRoutes is a placeholder function for setting up routes
-func InitRoutes() {
-	fmt.Println("Routes not implemented yet.")
+func RegisterRoutes() *mux.Router {
+	// Initialize the router
+	router := mux.NewRouter()
+
+	// User Routes
+	router.HandleFunc("/api/users", handlers.GetUsers).Methods("GET")
+	router.HandleFunc("/api/users", handlers.CreateUser).Methods("POST")
+	router.HandleFunc("/api/users/{id:[0-9]+}", handlers.GetUser).Methods("GET")
+	router.HandleFunc("/api/users/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
+	router.HandleFunc("/api/users/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
+
+	return router
 }
