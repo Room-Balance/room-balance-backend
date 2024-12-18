634460c827a184d0431d75f75403ffc7fd18d582
Batuhan Özender <b.ozender59@gmail.com>
2024-12-18 07:22:52 +0300

    Implement user handler functions

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -1,8 +1,84 @@
 package handlers
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
 
-// UserHandler is a placeholder handler
-func UserHandler() {
-	fmt.Println("User handler is not implemented yet.")
+	"strconv"
+
+	"github.com/Room-Balance/room-balance-backend.git/db"
+	"github.com/Room-Balance/room-balance-backend.git/db/models"
+	"github.com/gorilla/mux"
+)
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	var users []models.User
+	if err := db.DB.Find(&users).Error; err != nil {
+		http.Error(w, "Failed to fetch users", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	var user models.User
+	if err := db.DB.First(&user, id).Error; err != nil {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+	if err := db.DB.Create(&user).Error; err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	var user models.User
+	if err := db.DB.First(&user, id).Error; err != nil {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	var updatedData models.User
+	if err := json.NewDecoder(r.Body).Decode(&updatedData); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	user.Name = updatedData.Name
+	user.Email = updatedData.Email
+
+	if err := db.DB.Save(&user).Error; err != nil {
+		http.Error(w, "Failed to update user", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	if err := db.DB.Delete(&models.User{}, id).Error; err != nil {
+		http.Error(w, "Failed to delete user", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
